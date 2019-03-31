5e5d722267250879cf7101da59fde571c450203f
xiongxianze <xiongxianze@zhuanzhuan.com>
2019-04-01 00:33:21 +0800

    commit

--- src/main/java/com/xxz/MainApplication.java
+++ src/main/java/com/xxz/MainApplication.java
@@ -9,5 +9,6 @@ public class MainApplication {
         System.out.println("hello world");
         System.out.println("hello world1");
         System.out.println("hello world1.5");
+        System.out.println("2");
     }
 }
