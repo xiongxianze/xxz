71037aaeac6ac99bc4ca1b4a7bad2d282ab6c4de
xiongxianze <xiongxianze@zhuanzhuan.com>
2019-03-31 23:14:18 +0800

    idea 初始提交

---
+++ src/main/java/com/xxz/MainApplication.java
@@ -0,0 +1,11 @@
+package com.xxz;
+
+/**
+ * Created by Administrator on 2019/3/31.
+ */
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
